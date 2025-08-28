f640754dedef97453da13879beb1a4dda133fadb
Edwards <mapei_@hotmail.com>
2025-08-29 00:59:22 +0100

    Add files via upload

--- src/ubx.c
+++ src/ubx.c
@@ -27,6 +27,7 @@
 #include <conio.h>
 #include "ubx.h"
 #include "ubxcb.h"
+#include "extra.h"
 
 
 
@@ -42,8 +43,11 @@ static const uint32_t baudRates[] = {9600, 9600*2, 9600*4, 9600*6, 115200, 11520
 #define COM_BAUD_460800			6
 #define COM_BAUD_921600			7
 
-#define COM_PORT				(2)
-#define COM_BAUD				COM_BAUD_230400
+//#define COM_PORT				(29)
+//#define COM_PORT				(5)
+//#define COM_PORT				(19)
+#define COM_PORT				(9)
+#define COM_BAUD				COM_BAUD_115200
 #define COM_BAUD_FWDEFAULT		COM_BAUD_9600
 #define COM_BAUD_LASTSAVED		COM_BAUD_115200
 
@@ -53,7 +57,7 @@ static const uint32_t baudRates[] = {9600, 9600*2, 9600*4, 9600*6, 115200, 11520
 static ubx_msg_t ubxRegTable;
 static gpsdata_t userData;
 static uint32_t msgCt;
-
+static uint32_t rxt = 0;
 
 
 typedef struct{
@@ -73,6 +77,91 @@ static const char *fixType[] = {
 	"GNSS+deadReck",
 	"Time only"
 };
+
+
+int nWeekdayOfMonth (int week, int wday, int month, int year, struct tm *result)
+{
+   memset(result, 0, sizeof(*result));
+   time_t t;
+
+   year -= 1900;
+   result->tm_year = year;
+   result->tm_mon  = month;
+   result->tm_mday = 1;
+
+   t = mktime(result);
+   if (t == (time_t)-1)
+      return 0;
+
+   if (wday != result->tm_wday)
+      result->tm_mday += wday - result->tm_wday + 7 * (wday < result->tm_wday);
+
+   result->tm_mday += 7 * week;
+
+   t = mktime(result);
+   if (t == (time_t)-1)
+      return 0;
+
+   return 1;
+}
+
+void adjustUTCToTimeZone (utcTime_t *gps, uint16_t tz, uint8_t tzdir)
+{
+	uint8_t maxDayInAnyMonth[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; //gps->Mth 1-12 (not zero)
+
+
+	if (gps->Yr%4 == 0){
+		maxDayInAnyMonth[2] = 29;
+	}//adjust for leapyear
+    
+	uint8_t maxDayUtcMth = maxDayInAnyMonth[gps->Mth];
+	uint8_t maxDayPrevMth = maxDayInAnyMonth[gps->Mth-1];
+        
+	// month before utc month
+	if (!maxDayPrevMth) maxDayPrevMth = 31;
+
+
+	uint16_t hr = (gps->Hm/100)*100;
+	uint16_t m = gps->Hm-hr;  //2115 --> 2100 hr and 15 min
+	
+	if (tzdir){ //adjusting forwards
+		tz += gps->Hm;
+		if (tz >= 24){
+			gps->Hm = tz-24;
+			gps->Day++;                //spill over to next day
+			
+			if (gps->Day > maxDayUtcMth){
+				gps->Day = 1;
+				gps->Mth++; //spill over to next month
+				
+				if (gps->Mth > 12){
+					gps->Mth = 1;
+					gps->Yr++;        //spill over to next year
+				}
+			}
+		}else{
+			gps->Hm = tz;
+		}
+	}else{ //adjusting backwards
+		if (tz > gps->Hm){
+			gps->Hm = (24-(tz-hr))+m;
+			gps->Day--;  // back to previous day
+			
+			if (gps->Day == 0){                             //back to previous month
+				gps->Mth--;
+				gps->Day = maxDayPrevMth;
+				
+				if (!gps->Mth){
+					gps->Mth = 12;                 //back to previous year
+					gps->Yr--;
+				}
+			}
+		}else{
+			gps->Hm -= tz;
+		}
+	}
+}
+
 
 static inline void payloadDumpCt ()
 {
@@ -94,11 +183,24 @@ void msgPostLowCb (void *opaque, const intptr_t unused)
 	//gpsdata_t *data = (gpsdata_t*)opaque;
 	printf("msgPostLowCb\n");
 }
+/*
+static bool IsDst (int day, int month, int dow)
+    {
+        if (month < 3 || month > 10)  return false; 
+        if (month > 3 && month < 10)  return true; 
 
+        int previousSunday = day - dow;
+
+        if (month == 3) return previousSunday >= 25;
+        if (month == 10) return previousSunday < 25;
+
+        return false; // this line never gonna happend
+    }
+*/    
 // once per second
 void msgPostMedCb (void *opaque, const intptr_t unused)
 {
-	payloadDumpCt();
+	//payloadDumpCt();
 	
 	gpsdata_t *data = (gpsdata_t*)opaque;
 	//printf("msgPostMedCb\n");
@@ -109,21 +211,50 @@ void msgPostMedCb (void *opaque, const intptr_t unused)
 
 	printf("fixType: %s\n", fixType[data->fix.type]);
 	printf("Sats: %i\n", data->fix.sats);
-	printf("hAcc: %.2f\n", data->fix.hAcc/100.0f);
+	printf("3D Acc: %.2f\n", data->fix.pAcc/100.0f);
+	printf("2D Acc: %.2f\n", data->fix.hAcc/100.0f);
 	printf("vAcc: %.2f\n", data->fix.vAcc/100.0f);
-
-	printf("Dop: h:%.2f, v:%.2f, p:%.2f, g:%.2f\n",
+	
+	printf("Dop: H:%.2f, V:%.2f, P:%.2f, G:%.2f\n",
 		data->dop.horizontal/100.0f, data->dop.vertical/100.0f, data->dop.position/100.0f, data->dop.geometric/100.0f);
+
+
+	int lastSunMarch = 0;
+	int lastSunOctober = 0;
+	struct tm result;
 	
-	printf("Date: %.i.%.02i.%i\n", data->date.day, data->date.month, data->date.year);
+	int resultOk = nWeekdayOfMonth(-1, 0/*Sunday*/, 3/*March*/, data->date.year, &result);
+	if (resultOk)
+		lastSunMarch = result.tm_mday;
+	resultOk = nWeekdayOfMonth(-1, 0/*Sunday*/, 10/*October*/, data->date.year, &result);
+	if (resultOk)
+		lastSunOctober = result.tm_mday;
+
+	if ((data->date.month == 3 && data->date.day >= lastSunMarch) || data->date.month > 3){
+		if ((data->date.month == 10 && data->date.day < lastSunOctober) || data->date.month < 10){
+			utcTime_t utc;
+			utc.Yr = data->date.year;
+			utc.Mth = data->date.month;
+			utc.Day = data->date.day;
+			utc.Hm = data->time.hour;			
+			
+			adjustUTCToTimeZone(&utc, 0001, 1);
+			data->time.hour = utc.Hm;
+			data->date.day = utc.Day;
+			data->date.month = utc.Mth;
+			data->date.year = utc.Yr;
+		}
+	}
+	
 	printf("Time: %.02i:%.02i:%.02i.%.2i\n", data->time.hour, data->time.min, data->time.sec, data->time.ms);
-	
+	printf("Date: %.i.%.02i.%i\n", data->date.day, data->date.month, data->date.year);
 	printf("Heading: %.2f\n", data->misc.heading/100.0f);
 	printf("Speed: %.2f km/h\n", data->misc.speed/100.0f);
-	printf("\n");
 
-	printf("rx msgCt: %i\n", msgCt);
-	msgCt = 0;	
+	printf("rx msgCt: %i, %i\n", msgCt, rxt);
+	printf("\n");
+	msgCt = 0;
+	rxt = 0;
 }
 
 // more than once per second
@@ -151,6 +282,23 @@ static inline uint16_t calcChkSum (const uint8_t *hex, const uint32_t htotal, ui
 	}
 	return (*c1<<8)|*c2;
 }
+/*
+uint16_t calcChkSum2 (const uint8_t *hex, const uint32_t htotal, uint16_t *c1, uint16_t *c2)
+{
+	*c1 = 0; *c2 = 0;
+	
+	for (int i = 0; i < htotal; i++){
+		*c1 = (*c1 + hex[i]) % 256 ;
+		*c2 = (*c2 + *c1) % 256;
+	}
+	
+	*c1 &= 0xFF;
+	*c2 &= 0xFF;
+	
+	printf("%X %X\n", *c1, *c2);
+
+	return (*c1<<8)|*c2;
+}*/
 
 static inline void serialClose (ubx_device_t *dev)
 {
@@ -193,7 +341,7 @@ int serialOpen (ubx_device_t *dev, const int port, const int baud)
 	DCB dcb;
  	GetCommState(hSerial, &dcb);
 	dcb.fRtsControl = RTS_CONTROL_DISABLE;
-	dcb.fDtrControl = DTR_CONTROL_DISABLE;
+	dcb.fDtrControl = DTR_CONTROL_ENABLE;
 	dcb.fOutX = dcb.fInX = FALSE;
 	dcb.fOutxCtsFlow = FALSE;
 	dcb.fOutxDsrFlow = 0;
@@ -205,15 +353,17 @@ int serialOpen (ubx_device_t *dev, const int port, const int baud)
 
 	COMMTIMEOUTS CommTimeouts;
  	GetCommTimeouts(hSerial, &CommTimeouts);
-	CommTimeouts.WriteTotalTimeoutConstant = 50;
+	CommTimeouts.WriteTotalTimeoutConstant = 200;
 	CommTimeouts.WriteTotalTimeoutMultiplier = 0;
 	
-	CommTimeouts.ReadTotalTimeoutConstant = 50;
+	CommTimeouts.ReadTotalTimeoutConstant = 200;
 	CommTimeouts.ReadTotalTimeoutMultiplier = 0;
 	CommTimeouts.ReadIntervalTimeout = MAXWORD;
 	SetCommTimeouts(hSerial, &CommTimeouts);
 
  	dev->u.serial = hSerial;
+	serialClean(dev);
+	Sleep(100);
 	serialClean(dev);
  	return 1;
 }
@@ -228,6 +378,7 @@ static inline int serialWrite (ubx_device_t *dev, void *buffer, const uint32_t b
 
 static inline int serialRead (ubx_device_t *dev, void *buffer, const uint32_t bufferSize, uint32_t *bytesRead)
 {
+	//int ret = ReadFile(dev->u.serial, buffer, bufferSize, (unsigned long*)bytesRead, NULL);
 	int ret = ReadFile(dev->u.serial, buffer, bufferSize, (unsigned long*)bytesRead, NULL);
 	//int er = GetLastError();
 	//printf("serialRead %i %i, %X\n", ret, *bytesRead, er);
@@ -241,15 +392,6 @@ static inline void bufferDump (const uint8_t *buffer, const int32_t bufferSize)
 		printf("0x%02X ", buffer[i]);
 
 	printf("\n");
-}
-
-static inline int findSync (const uint8_t *buffer, const int32_t bufferSize)
-{
-	for (int i = 0; i < bufferSize-6; i++){
-		if (buffer[i] == MSG_UBX_B1 && buffer[i+1] == MSG_UBX_B2)
-			return i;
-	}
-	return -1;
 }
 
 static inline ubx_func_t *payloadHandlerFuncGet (uint8_t msg_class, uint8_t msg_id)
@@ -382,17 +524,17 @@ static inline void ubxPayloadDispatch (uint8_t msg_class, uint8_t msg_id, uint16
 	}
 }
 
-static inline int ubxBufferFrameDecompose (uint8_t *buffer, const int32_t bufferSize)
+int ubxBufferFrameProcess (uint8_t *buffer, const int32_t bufferSize)
 {
 	const uint8_t msg_class = buffer[0];
 	const uint8_t msg_id = buffer[1];
 	const uint16_t msg_len = (buffer[3]<<8) | buffer[2];
 	const uint16_t msg_crc = (buffer[3+msg_len+1]<<8) | buffer[3+msg_len+2];
 
-	int frameEnd = 3+msg_len+2; 
+	int frameEnd = 4+msg_len+2; 
 	if (frameEnd > bufferSize){
-		//printf("multipart frame: %i %i\n", frameEnd, bufferSize);
-		return -frameEnd;
+		printf("multipart frame: %i %i\n", frameEnd, bufferSize);
+		return -1;
 	}
 	
 	//printf("class: %.2X/%.2X\n", msg_class, msg_id);
@@ -402,31 +544,17 @@ static inline int ubxBufferFrameDecompose (uint8_t *buffer, const int32_t buffer
 	uint8_t c2 = 0;
 	const uint16_t crc = calcChkSum(buffer, msg_len+4, &c1, &c2);
 	if (msg_crc != crc){
+		printf("class: %.2X/%.2X\n", msg_class, msg_id);
+		printf("len: %i\n", msg_len);
 		printf("crc mismatch: %X, %X\n", msg_crc, crc);
 		return 0;
 	}	
-
-	//printf("crc: %X, %X\n", msg_crc, crc);
+	//printf("CRC: %X, %X, %i\n", msg_crc, crc, msg_len+4+2);
 	
 	//we have a valid frame
 	ubxPayloadDispatch(msg_class, msg_id, msg_len, &buffer[4]);
 	
-	return msg_len+4+2;	// 
-}
-
-static inline int ubxBufferDecompose (uint8_t *buffer, const int32_t bufferSize, uint32_t *ct)
-{
-	for (int i = 0; i < bufferSize-6; ){
-		int found = findSync(&buffer[i], bufferSize-i);
-		if (found < 0) return -i;
-		i += found + 2;
-		
-		(*ct)++;
-		int ret = ubxBufferFrameDecompose(&buffer[i], bufferSize-i);
-		if (ret < 0) return -(i-2);
-		i += ret;
-	}
-	return 1;
+	return msg_len+4+2;
 }
 
 static inline int writeBin (ubx_device_t *dev, void *buffer, const uint32_t bufferSize)
@@ -553,39 +681,100 @@ static inline void disableUnknowns (ubx_device_t *dev)
 #endif
 }
 
-static inline uint32_t ubxWinRun (ubx_device_t *dev, uint8_t *buffer, const uint32_t bufferSize, uint32_t *bOffset)
+int searchSync (uint8_t *buffer, uint32_t len)
 {
- 	memset(buffer+*bOffset, 0, bufferSize-*bOffset);
- 	uint32_t bytesRead = 0;
- 	uint32_t ct = 0;
-
-	int ret = serialRead(dev, buffer+*bOffset, bufferSize-*bOffset, &bytesRead);
-	if (ret > 0){
-		bytesRead += *bOffset;
-		*bOffset = 0;
-
-		ret = ubxBufferDecompose(buffer, bytesRead, &ct);
-		if (ret < 0){
-			*bOffset = -ret;
-			memcpy(buffer, &buffer[*bOffset], (bytesRead - *bOffset)+1);
-			*bOffset = (bytesRead - *bOffset);
+	if (len > 1){
+		for (int i = 0; i < len-1; i++){
+			if (buffer[i] == MSG_UBX_B1 && buffer[i+1] == MSG_UBX_B2)
+				return i;
 		}
-	}else{
-		Sleep(10);
 	}
-	return ct;
+	return -1;
+}
+
+int searchFrame (uint8_t *buffer, const int32_t bufferSize)
+{
+	//const uint8_t msg_class = buffer[0];
+	//const uint8_t msg_id = buffer[1];
+	const uint16_t msg_len = (buffer[3]<<8) | buffer[2];
+	const uint16_t msg_crc = (buffer[3+msg_len+1]<<8) | buffer[3+msg_len+2];
+
+	int frameEnd = 4+msg_len+2; 
+	if (frameEnd > bufferSize)
+		return 0;
+	
+	uint8_t c1 = 0;
+	uint8_t c2 = 0;
+	const uint16_t crc = calcChkSum(buffer, msg_len+4, &c1, &c2);
+	if (msg_crc != crc)
+		return -(msg_len+4+2);
+	
+	return msg_len+4+2;
+}
+
+uint32_t ubxMsgRun (ubx_device_t *dev, uint8_t *inBuffer, uint32_t bufferSize, int32_t *writePos)
+{
+	uint32_t writeLength = 0;
+	uint32_t bytesRead = 0;
+	uint8_t readBuffer[8] = {0}; // Should be no larager than the smallest [message+control bytes]
+
+
+	serialRead(dev, readBuffer, sizeof(readBuffer), &bytesRead);
+	if (bytesRead > 0){
+		rxt += bytesRead;
+
+		for (int i = 0; i < bytesRead; i++)
+			inBuffer[(*writePos)++] = readBuffer[i];
+
+		writeLength = *writePos;
+		if (writeLength < 6) return 0;
+		
+		// check if there is a complete message
+		int foundSync = searchSync(inBuffer, writeLength);
+		if (foundSync < 0){
+			printf("sync not found %i %i\n", foundSync, writeLength);
+			//bufferDump(inBuffer, writeLength);
+			return 0;
+		}
+
+		// advance past sync bytes
+		foundSync += 2;
+
+		int foundFrame = searchFrame(&inBuffer[foundSync], writeLength-foundSync);
+		if (foundFrame > 0){
+			int process = ubxBufferFrameProcess(&inBuffer[foundSync], writeLength-foundSync);
+
+			if (foundSync+foundFrame < writeLength){
+				for (int i = 0; i < sizeof(readBuffer); i++)
+					inBuffer[i] = inBuffer[foundSync+foundFrame+i];
+
+				*writePos = writeLength - (foundSync+foundFrame);
+				memset(inBuffer+sizeof(readBuffer), 0, bufferSize-sizeof(readBuffer));
+			}else{
+				memset(inBuffer, 0, bufferSize);
+				*writePos = 0;
+			}
+			return (process > 0);
+
+		}else if (foundFrame < 0){
+			printf("mismatch crc %i, %i %i\n", -foundFrame, *writePos, foundSync);
+			memset(inBuffer, 0, bufferSize);
+			*writePos = 0;
+		}
+	}
+	return 0;
 }
 
 unsigned int __stdcall readThread (void *ptr)
 {
 	ubx_device_t *dev = (ubx_device_t*)ptr;
 	
-	const uint32_t bufferSize = 8192 * 16;
-	uint8_t buffer[bufferSize];
-	uint32_t bOffset = 0;
+	const uint32_t bufferSize = 2048;
+	uint8_t buffer[bufferSize+8];
+	int32_t bOffset = 0;
  		
 	while (!kbhit())
-		msgCt += ubxWinRun(dev, buffer, bufferSize, &bOffset);
+		msgCt += ubxMsgRun(dev, buffer, bufferSize, &bOffset);
 	
 	_endthreadex(1);
 	return 1;
@@ -638,21 +827,21 @@ static inline void configureGNSS (ubx_device_t *dev)
 	cfg = &gnss->cfgblk[1];
 	cfg->gnssId = GNSSID_GLONASS;
 	cfg->resTrkCh = 4;
-	cfg->maxTrkCh = 20;
+	cfg->maxTrkCh = 24;
 	cfg->flags = GNSS_CFGBLK_ENABLED | GNSS_CFGBLK_SIGENABLED;
 	
 	cfg = &gnss->cfgblk[2];
 	cfg->gnssId = GNSSID_GALILEO;
 	cfg->resTrkCh = 4;
 	cfg->maxTrkCh = 10;
-	cfg->flags = GNSS_CFGBLK_DISABLED | GNSS_CFGBLK_SIGENABLED;
+	cfg->flags = GNSS_CFGBLK_ENABLED | GNSS_CFGBLK_SIGENABLED;
 
 	cfg = &gnss->cfgblk[3];
 	cfg->gnssId = GNSSID_BEIDOU;
 	cfg->resTrkCh = 8;
 	cfg->maxTrkCh = 16;
 	cfg->flags = GNSS_CFGBLK_DISABLED | GNSS_CFGBLK_SIGENABLED;
-	
+
 	cfg = &gnss->cfgblk[4];
 	cfg->gnssId = GNSSID_IMES;
 	cfg->resTrkCh = 0;
@@ -672,7 +861,7 @@ static inline void configureGNSS (ubx_device_t *dev)
 	cfg->flags = GNSS_CFGBLK_DISABLED | GNSS_CFGBLK_SIGENABLED;
 
 	gnss->msgVer = 0;
-	gnss->numTrkChHw = 32;
+	gnss->numTrkChHw = 72;
 	gnss->numTrkChUse = 32;
 	gnss->numConfigBlocks = cfgBlks;
 
@@ -699,7 +888,7 @@ static inline void configureRate (ubx_device_t *dev)
 {
 	cfg_rate_t rate = {0};
 	
-	rate.measRate = 53;		// ms. 53ms = ~18-19hz
+	rate.measRate = 56;		// ms. 53ms = ~18-19hz
 	rate.navRate = 1;		// 1 measurement per navigation
 	rate.timeRef = CFG_TIMEREF_UTC;
 	
@@ -716,7 +905,7 @@ static inline void configureNav5 (ubx_device_t *dev)
 		
 		
 	nav.dynModel = NAV5_DYNMODEL_STATIONARY;
-	nav.fixMode = NAV5_FIXMODE_3D;
+	nav.fixMode = NAV5_FIXMODE_AUTO;
 	nav.fixedAlt = 37.0f * 100;				// meters, when using NAV5_FIXMODE_2D
 	nav.fixedAltVar = 0.5f * 10000;			// deviation,  ^^^ 
 	nav.minElv = 5;
@@ -879,7 +1068,6 @@ static inline void baudReset (ubx_device_t *dev)
 	}
 }
 
-
 int main (const int argc, const char *argv[])
 {   
 
@@ -892,6 +1080,7 @@ int main (const int argc, const char *argv[])
 	payloadHandlerSet(UBX_ACK, UBX_ACK_NAK, ack_nak, 0);
 	payloadHandlerSet(UBX_ACK, UBX_ACK_ACK, ack_ack, 0);
 	payloadHandlerSet(UBX_NAV, UBX_NAV_GEOFENCE, nav_geofence, MSG_STATUS_ENABLED);
+	payloadHandlerSet(UBX_NAV, UBX_NAV_POSECEF, nav_posecef, MSG_STATUS_ENABLED);
 	payloadHandlerSet(UBX_NAV, UBX_NAV_DOP, nav_dop, MSG_STATUS_ENABLED);
 	payloadHandlerSet(UBX_NAV, UBX_NAV_PVT, nav_pvt, MSG_STATUS_ENABLED);
 	payloadHandlerSet(UBX_NAV, UBX_NAV_SVINFO, nav_svinfo, MSG_STATUS_ENABLED);
@@ -911,64 +1100,74 @@ int main (const int argc, const char *argv[])
 	payloadHandlerSet(UBX_INF, UBX_INF_TEST, inf_debug, MSG_STATUS_ENABLED);
 	payloadHandlerSet(UBX_INF, UBX_INF_DEBUG, inf_debug, MSG_STATUS_ENABLED);	
 	payloadHandlerSet(UBX_CFG, UBX_CFG_INF, cfg_inf, MSG_STATUS_ENABLED);
+	payloadHandlerSet(UBX_CFG, UBX_CFG_RATE, cfg_rate, MSG_STATUS_ENABLED);
 	payloadHandlerSet(UBX_CFG, UBX_CFG_NAV5, cfg_nav5, MSG_STATUS_ENABLED);	
 	payloadHandlerSet(UBX_CFG, UBX_CFG_NAVX5, cfg_navx5, MSG_STATUS_ENABLED);	
+	payloadHandlerSet(UBX_CFG, UBX_CFG_GNSS, cfg_gnss, MSG_STATUS_ENABLED);	
 	payloadHandlerSet(UBX_CFG, UBX_CFG_GEOFENCE, cfg_geofence, MSG_STATUS_ENABLED);
 	payloadHandlerSet(UBX_CFG, UBX_CFG_PRT, cfg_prt, MSG_STATUS_ENABLED);
 	payloadHandlerSet(UBX_CFG, UBX_CFG_USB, cfg_usb, MSG_STATUS_ENABLED);
+	payloadHandlerSet(UBX_NAV, UBX_NAV_TIMEBDS, nav_timebds, MSG_STATUS_ENABLED);
+	
 
 
 	ubx_device_t dev = {0};
 	baudReset(&dev);
 
 	if (serialOpen(&dev, COM_PORT, BAUDRATE(COM_BAUD))){
+		printf("Port %i Open @ %i\n\n", COM_PORT, BAUDRATE(COM_BAUD));
+		
 		unsigned int tid = 0;
 		HANDLE hReadThread = (HANDLE)_beginthreadex(NULL, 0, readThread, &dev, 0, &tid);
 		Sleep(10);
 
-		if (1) configurePorts(&dev);
-		if (1) configureInf(&dev);
-		if (1) configureRate(&dev);
-		if (1) configureNav5(&dev);
-		if (1) configureNavX5(&dev);
+		if (0) configurePorts(&dev);
+		if (0) configureInf(&dev);
+		if (0) configureRate(&dev);
+		if (0) configureNav5(&dev);
+		if (0) configureNavX5(&dev);
 		if (0) configureGNSS(&dev);		// will auto generate a warm-start
 		if (0) configureGeofence(&dev);
 
 		ubx_msgDisableAll(&dev);
+		ubx_msgEnable(&dev, UBX_MON, UBX_MON_IO);
 		ubx_msgPoll(&dev, UBX_MON, UBX_MON_VER);
 		ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_USB);
 		ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_PRT);
-		ubx_msgEnable(&dev, UBX_MON, UBX_MON_IO);
 		ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_DOP);
 		ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_PVT);
-		ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_EOE);
+		//ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_EOE);
+		ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_POSECEF);
+		
 		//ubx_msgEnableEx(&dev, UBX_NAV, UBX_NAV_GEOFENCE, 60);
-		ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_GEOFENCE);
-		//ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_NAV5);
+		//ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_GEOFENCE);
 		//ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_NAVX5);
-		//ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_SAT);
-		//ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_SVINFO);
+		ubx_msgEnableEx(&dev, UBX_NAV, UBX_NAV_SAT, 5);
+		//ubx_msgEnableEx(&dev, UBX_NAV, UBX_NAV_SVINFO, 7);
 		//ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_STATUS);
 		//ubx_msgPoll(&dev, UBX_AID, UBX_AID_EPH);
 		//ubx_msgPoll(&dev, UBX_AID, UBX_AID_ALM);
 		//ubx_msgPoll(&dev, UBX_AID, UBX_AID_AOP);
 		//ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_POSLLH);
-
+		ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_GNSS);
+		ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_NAV5);
+		ubx_msgPoll(&dev, UBX_CFG, UBX_CFG_RATE);
+		
 
 		while(!kbhit()){
 			// do something
-			Sleep(500);
+			Sleep(50);
 		}
-
 	
 		WaitForSingleObject(hReadThread, INFINITE);
 		CloseHandle(hReadThread);
 	
-		ubx_msgDisable(&dev, UBX_MON, UBX_MON_IO);
-		ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_SAT);
+		//ubx_msgDisable(&dev, UBX_MON, UBX_MON_IO);
+		//ubx_msgEnable(&dev, UBX_NAV, UBX_NAV_SAT);
 		Sleep(20);
 		
 		serialClean(&dev);
+		Sleep(20);
 		serialClose(&dev);
 	}else{
 		printf("could not open serial port %i\n", COM_PORT);
